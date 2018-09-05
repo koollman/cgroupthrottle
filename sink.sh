e41122b6d006f755f1cd1bcae9495306f98649e7
Thomas Samson <tsamson@advanced-schema.com>
2018-09-05 16:51:59 +0200

    need to cap value

--- sink.sh
+++ sink.sh
@@ -3,14 +3,20 @@
 cores=$(nproc)
 
 max=$(cgget -nvr cpu.cfs_period_us $(cat conf/cgroup))
+max_allcores=$((max*cores))
 val=$max
 
 echo "cores: $cores, max: $max"
 while read adjust; do
     newadjust=$((adjust*cores))
     newval=$((val+newadjust))
+    if [ $newval -lt 0 ]; then
+	newval=0
+    elif [ $newval -gt $max_allcores ]; then
+	newval=$max_allcores
+    fi
     ratio=$((newval/max))
-    echo "got $adjust, setting to $newval / $max ($ratio)"
+    echo "got $adjust, setting to $newval / $max_allcores ($ratio)"
     cgset -r cpu.cfs_quota_us=$newval $(cat conf/cgroup)
     val=$newval
 done
